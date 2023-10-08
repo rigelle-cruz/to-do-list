3963e4e20d05111696b057f8a171ae9533a3f1d6
Rigelle Cruz <rigellecruz@gmail.com>
2023-10-09 10:11:28 +1300

    create form in AddTodo component

--- client/components/AddTodo.tsx
+++ client/components/AddTodo.tsx
@@ -1,7 +1,42 @@
+import { useState, ChangeEvent, FormEvent } from 'react'
+import { useAppDispatch } from '../hooks'
+import { postTaskThenFetch } from '../slices/todos'
 
+interface InputState {
+  task: string
+}
 
 function AddTodo() {
-  
+  const dispatch = useAppDispatch()
+
+  const [input, setInput] = useState<InputState>({
+    task: '',
+  })
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInput({
+      ...input,
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  const handleFormSubmit = (event: FormEvent) => {
+    event.preventDefault()
+    dispatch(postTaskThenFetch(input))
+  }
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <input
+        type="text"
+        name="task"
+        className="new-todo"
+        placeholder="What needs to be done?"
+        value={input.task}
+        onChange={handleInputChange}
+      />
+    </form>
+  )
 }
 
 export default AddTodo
