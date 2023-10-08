34387db9200d80f482e3e9690cfccb478ac9cada
Rigelle Cruz <rigellecruz@gmail.com>
2023-10-09 09:26:51 +1300

    create App component

---
+++ client/components/App.tsx
@@ -0,0 +1,17 @@
+import Todos from './Todos'
+import AddTodo from './AddTodo'
+
+function App() {
+  return (
+    <>
+      <header className="header">
+        <h1>todos</h1>
+        
+      </header>
+      <section className="main"></section>
+      <footer className="footer"></footer>
+    </>
+  )
+}
+
+export default App
