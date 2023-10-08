2d7dad1c573af2d08dcd1ea7347d2e3601c16936
Rigelle Cruz <rigellecruz@gmail.com>
2023-10-09 10:19:39 +1300

    display list

--- client/components/Todos.tsx
+++ client/components/Todos.tsx
@@ -1,6 +1,47 @@
+import { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from '../hooks'
+import { fetchTasks, deleteTaskThenFetch } from '../slices/todos'
+import { Task } from '../../models/Todos'
 
 function Todos() {
- 
+  const dispatch = useAppDispatch()
+  const tasks = useAppSelector((state) => state.todos)
+
+  useEffect(() => {
+    dispatch(fetchTasks())
+  }, [dispatch])
+
+  const handleDelete = (id: number) => {
+    dispatch(deleteTaskThenFetch(id))
+  }
+
+  return (
+    <ul>
+      {tasks.map((task: Task) => (
+        <li
+          style={{
+            fontSize: '20px',
+            listStyleType: 'none',
+            padding: '10px',
+            display: 'flex',
+            alignItems: 'center',
+          }}
+          key={task.id}
+        >
+          <label>
+            <input type="checkbox" className="checkbox" />
+            {task.task}
+            <button
+              className="delete-button"
+              onClick={() => handleDelete(task.id)}
+            >
+              x
+            </button>
+          </label>
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 export default Todos
